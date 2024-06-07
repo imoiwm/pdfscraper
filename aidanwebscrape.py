eaa09e2310006d17c5d1b283117866e680b86d2c
imoiwm <imoiwm@gmail.com>
2024-06-07 02:30:18 -0400

    Updated to work with an inputted provider number rather than one exact provider URL that was used for MVP

--- aidanwebscrape.py
+++ aidanwebscrape.py
@@ -1,35 +1,17 @@
-"""# Parse text obtained
-soup = BeautifulSoup(response.text, 'html.parser')
- 
-# Find all hyperlinks present on webpage
-links = soup.find_all('a')
- 
-i = 0
- 
-# From all links check for pdf link and
-# if present download file
-for link in links:
-    if ('.pdf' in link.get('href', [])):
-        i += 1
-        print("Downloading file: ", i)
- 
-        # Get response object for link
-        response = requests.get(link.get('href'))
- 
-        # Write content in pdf file
-        pdf = open(str(link)+".pdf", 'wb')
-        pdf.write(response.content)
-        pdf.close()
-        print("File ", i, " downloaded")
- """
-
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
 
+"""
+Taken from 
+https://stackoverflow.com/questions/34338897/python-selenium-find-out-when-a-download-has-completed
+
+Waits until Chrome download of PDF has completed
+or 20 seconds have passed
+in order to avoid incomplete download files being left behind
+"""
 def download_wait():
     seconds = 0
     dl_wait = True
@@ -42,15 +24,43 @@ def download_wait():
         seconds += 1
     return seconds
 
+provider_number = input("Please input the DECAL provider number: ")
+
+# Prefix URLs for the search results and the provider breakdown pages, respectively
+search_url = "https://families.decal.ga.gov/ChildCare/Results?name="
+base_url = "https://families.decal.ga.gov/ChildCare/"
+
+
 driver = webdriver.Chrome()
-driver.get("https://families.decal.ga.gov/ChildCare/detail/15173")
-#assert "Python" in driver.title
-for b in driver.find_elements(By.XPATH, "//a[@title='View Inspection Report']"):
-    print("what")
-    print(b)
-    b.click()
-assert "No results found." not in driver.page_source
-download_wait()
+driver.get(search_url + provider_number)
+
+"""
+Handles case of multiple search results for provider number
+just as precaution
+
+Otherwise, main purpose is to grab the link 
+from clicking on the "View" button in the search results
+"""
+detail_suffix = []
+for result in driver.find_elements(By.XPATH, "//a[@data-track-name='View Provider - Button']"):
+    detail_suffix.append(result.get_attribute('href'))
+
+# For each URL grabbed from the "View" buttons in the search results
+for ds in detail_suffix:
+    # Left in a total recorded count of PDFs downloaded for troubleshooting purposes
+    count = 0
+
+    # Generate the page from the specified URL
+    driver.get(ds)
+
+    # Have Selenium click to download each PDF under the "Inspection Report" section
+    for b in driver.find_elements(By.XPATH, "//a[@title='View Inspection Report']"):
+        count += 1
+        b.click()
+    assert "No results found." not in driver.page_source
+
+    # Wait to make sure all downloads complete
+    download_wait()
+    print(f"Downloaded {count} files from provider number {provider_number}")
 driver.close()
 
-print("All PDF files downloaded")
