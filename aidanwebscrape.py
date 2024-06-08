6fa3ab375997c159d979265af21368d319edc8fc
imoiwm <imoiwm@gmail.com>
2024-06-08 16:17:34 -0400

    WIP converting to accepting .csv file to mass download from list of provider_numbers. Also added in some extra documentation/program execution control

--- aidanwebscrape.py
+++ aidanwebscrape.py
@@ -3,6 +3,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
+import csv
+
+"""
+This program is run by downloading the Python file
+and then running it using the following command
+(note the $ at the beginning is to denote the command line):
+
+$ python aidanwebscrape.py {.csv filename}
+
+
+TODO: see if we can move the files to a specific folder instead of just Downloads
+"""
 
 """
 Taken from 
@@ -24,43 +36,62 @@ def download_wait():
         seconds += 1
     return seconds
 
-provider_number = input("Please input the DECAL provider number: ")
-
-# Prefix URLs for the search results and the provider breakdown pages, respectively
-search_url = "https://families.decal.ga.gov/ChildCare/Results?name="
-base_url = "https://families.decal.ga.gov/ChildCare/"
-
-
-driver = webdriver.Chrome()
-driver.get(search_url + provider_number)
+def providers_from_csv(filename):
+    res = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            res.append(row['provider_number'])
 
 """
-Handles case of multiple search results for provider number
-just as precaution
+Where the primary logic is performed
+Scrapes all PDFs and downloads them into the same directory as this Python program
+given a .csv file containing the fieldnames 'Count,' 'Provider_Number', and 'Location' in the first line
+followed by each entry's three fieldname parameters on each new line
 
-Otherwise, main purpose is to grab the link 
-from clicking on the "View" button in the search results
+Note that for your purposes with DECAL, 
+this can be done by exporting the spreadsheet in Google Sheets
+(didn't test for Microsoft Excel) as a .csv
 """
-detail_suffix = []
-for result in driver.find_elements(By.XPATH, "//a[@data-track-name='View Provider - Button']"):
-    detail_suffix.append(result.get_attribute('href'))
+def decal_pdfs(providers):
+    # Prefix URLs for the search results and the provider breakdown pages, respectively
+    search_url = "https://families.decal.ga.gov/ChildCare/Results?name="
+    base_url = "https://families.decal.ga.gov/ChildCare/"
 
-# For each URL grabbed from the "View" buttons in the search results
-for ds in detail_suffix:
-    # Left in a total recorded count of PDFs downloaded for troubleshooting purposes
-    count = 0
+    driver = webdriver.Chrome()
+    for pn in providers:
+        driver.get(search_url + pn)
 
-    # Generate the page from the specified URL
-    driver.get(ds)
+        # Handles case of multiple search results for provider number just as precaution
+        # Otherwise, main purpose is to grab the link from clicking on the "View" button in the search results
+        detail_suffix = []
+        for result in driver.find_elements(By.XPATH, "//a[@data-track-name='View Provider - Button']"):
+            detail_suffix.append(result.get_attribute('href'))
 
-    # Have Selenium click to download each PDF under the "Inspection Report" section
-    for b in driver.find_elements(By.XPATH, "//a[@title='View Inspection Report']"):
-        count += 1
-        b.click()
-    assert "No results found." not in driver.page_source
+        # For each URL grabbed from the "View" buttons in the search results
+        for ds in detail_suffix:
+            # Left in a total recorded count of PDFs downloaded for troubleshooting purposes
+            count = 0
 
-    # Wait to make sure all downloads complete
-    download_wait()
-    print(f"Downloaded {count} files from provider number {provider_number}")
-driver.close()
+            # Generate the page from the specified URL
+            driver.get(ds)
 
+            # Have Selenium click to download each PDF under the "Inspection Report" section
+            for b in driver.find_elements(By.XPATH, "//a[@title='View Inspection Report']"):
+                count += 1
+                b.click()
+            assert "No results found." not in driver.page_source
+
+            # Wait to make sure all downloads complete
+            download_wait()
+            print(f"Downloaded {count} files from provider number {pn}")
+        driver.close()
+
+
+if __name__ == "__main__":
+    #provider_number = input("Please input the DECAL provider number: ")
+
+    #TODO this line takes in the filename passed as a function parameter
+    #TODO need to test this program on a smaller subset of the initial .csv
+    provider_numbers = providers_from_csv(filename)
+    decal_pdfs(provider_numbers)
